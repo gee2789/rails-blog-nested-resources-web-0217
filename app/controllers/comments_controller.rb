777b967255979853f5220d3899fae7e387fe11a0
gee2789 <geeyung.li@gmail.com>
2017-03-07 13:36:16 -0500

    Done.

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,22 @@
+class CommentsController < ApplicationController
+
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @comment= Comment.create(comment_params)
+    redirect_to post_path(@comment.post_id)
+  end
+
+  def show
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content, :user_id, :post_id)
+  end
+
+
+end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,6 +6,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @comments = Comment.new
   end
 
   def new
